8953845788e19c7dcf2ca7700304cb1eeb3b30bf
NazrinMammadova <102550859+NazrinMammadova@users.noreply.github.com>
2022-07-11 19:39:23 +0400

    added task

---
+++ js/ap.js
@@ -0,0 +1,10 @@
+
+
+inputt = document.getElementsByClassName("one")[0]
+btn = document.getElementsByClassName("btn")[0]
+
+
+btn.addEventListener("click",function () {
+    inputt.value = inputt.value.toUpperCase();
+}) 
+
